d3fda081110ab2c2539684417b689732ee8291f4
Gautam Jain <rpgautam.jain@gmail.com>
2025-01-17 01:30:14 +0530

    Parent Child

---
+++ 7.ParentChild/script.js
@@ -0,0 +1,11 @@
+function setup(){
+    var canvas = createCanvas(400,400);
+    canvas.parent('canvasp');
+
+}
+
+function draw(){
+    background(0);
+    ellipse(200,200,random(100),random(100));
+       
+}
