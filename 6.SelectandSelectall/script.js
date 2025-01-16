bc77e03d2bddfd1db9a9cbee4b768591c96991a9
Gautam Jain <rpgautam.jain@gmail.com>
2025-01-16 23:42:07 +0530

    8.11 callback on multiple dom elements, hovering animation kind off

--- 6.SelectandSelectall/script.js
+++ 6.SelectandSelectall/script.js
@@ -4,19 +4,37 @@ var paragraphs;
 function setup(){
     createCanvas(400,200);
     background(0);
-    // paragraph=select('#unicorn');
-    // paragraphs=selectAll('p');
-    paragraphs=selectAll('.rainbow');
-    for(var i=0;i<paragraphs.length;i++){
-        paragraphs[i].style('font-size','24pt');
+
+    for (var i = 0; i < 100; i++){
+        var par = createP('rainbow!');
+        par.position(random(500),random(500));
     }
-    // paragraph.mouseOver(changeBackground);
+
+
+    paragraph=select('#unicorn');
+    paragraphs=selectAll('p');
+    // paragraphs=selectAll('.rainbow');
+    for(var i=0;i<paragraphs.length;i++){
+        paragraphs[i].style('font-size','14pt');
+        paragraphs[i].mouseOver(highlight);
+        paragraphs[i].mouseOut(unhighlight);
+    }
+
+    
+    paragraph.mouseOver(highlight);
+    paragraph.mouseOut(unhighlight);
     var button=select('#button');
     button.mousePressed(canvasBg);
 }
 
-function changeBackground(){
-    paragraph.style('background-color','#F0F')
+function highlight(){
+    this.style('padding','16pt');
+    this.style('background-color','#F0F');
+
+}
+function unhighlight(){
+    this.style('padding','0pt');
+    this.style('background-color','#FFF');
 
 }
 
