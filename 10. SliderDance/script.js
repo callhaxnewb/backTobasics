d324c30ee7c952f747206f7fe173a13a56a2e50e
Gautam Jain <rpgautam.jain@gmail.com>
2025-01-19 17:45:23 +0530

    One ring to rule them all

--- 10. SliderDance/script.js
+++ 10. SliderDance/script.js
@@ -8,19 +8,34 @@ function setup(){
         sliders[i]=createSlider(0,255,50);
     }
 
+    sliders[0].input(adjustSliders);
+
 }
 
-function draw(){
-    var offset=0  ;
-    for( var i = 0; i<sliders.length;i++){
-        var x=map(sin(angle+offset),-1,1,0,255); 
-        var y=map(sin(angle+offset),-1,1,255,0);
-        if(i%2==0){
-            sliders[i].value(x);
-        }else{
-            sliders[i].value(y);
-        }
-        offset+=0.025;   
+function adjustSliders(){
+    var offset=0;
+    for( var i = 0; i<sliders.length/2;i++){
+        sliders[i].value(sliders[0].value()+offset);
+        offset+=1    
     }
-    angle+=0.1
+    for( var i = sliders.length/2; i<sliders.length;i++){
+        sliders[i].value(255-(sliders[0].value()-offset));
+        offset+=1    
+    }
+
+ 
 }
+// function draw(){
+//     var offset=0  ;
+//     for( var i = 0; i<sliders.length;i++){
+//         var x=map(sin(angle+offset),-1,1,0,255); 
+//         var y=map(sin(angle+offset),-1,1,255,0);
+//         if(i%2==0){
+//             sliders[i].value(x);
+//         }else{
+//             sliders[i].value(y);
+//         }
+//         offset+=0.025;   
+//     }
+//     angle+=0.1
+// }
