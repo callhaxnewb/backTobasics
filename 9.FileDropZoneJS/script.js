f0b1bf3352a528345a3ee302cebacfa505f298a4
Gautam Jain <rpgautam.jain@gmail.com>
2025-01-24 01:05:43 +0530

    Drop zone!!

---
+++ 9.FileDropZoneJS/script.js
@@ -0,0 +1,26 @@
+var dropzone;
+
+function setup(){
+    createCanvas(200,200);
+    background(0);
+
+    dropzone=select('#dropzone');
+    dropzone.dragOver(highlight);
+    dropzone.dragLeave(unhighlight);
+    dropzone.drop(gotFile,unhighlight)
+}
+
+function gotFile(file){
+    createP;(file.name);
+    createP(file.type);
+    createP(file.size);
+    createImg(file.data);
+}
+
+function highlight(){
+    dropzone.style('background-color','#CCC');
+}
+
+function unhighlight(){
+    dropzone.style('background-color','#fff');
+}
